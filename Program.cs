2f2ce954087ddb736d9fffb631b3df9cf8862459
Davina Shah <davina.shah@asos.com>
2018-06-27 14:36:55 +0100

    Revert "added an endless loop"
    This reverts commit 64062bf48aaabcf8be3fa1d61787899cb078c868.

--- Program.cs
+++ Program.cs
@@ -7,10 +7,6 @@ namespace CalculatorProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Calculator Version 1.0");
-            while(true)
-            {
-                Console.WriteLine("Endless looop!");
-            }
         }
     }
 }
