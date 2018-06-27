dd3694e0389017d68776682249eb411bdba2501c
Davina Shah <davina.shah@asos.com>
2018-06-27 14:11:18 +0100

    added initial version of the calc program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatorProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator Version 1.0");
+        }
+    }
+}
