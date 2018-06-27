0eb5a9fcddf6d35f518587e05a1c54bd16c11c3d
Davina Shah <davina.shah@asos.com>
2018-06-27 15:24:51 +0100

    Added bug fixes

--- Program.cs
+++ Program.cs
@@ -4,9 +4,13 @@ namespace TheCalculatorProgram
 {
     class Program
     {
+        public static void SomeImportantBugFix(){
+            //fixes ALL the bugs 
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Calculator Version 1.0");
+            SomeImportantBugFix();
         }
     }
 }
